e5de6ce9371e0ccffd012dbde737ee7c06ec7196
codebelt <code@codebelt.com>
2017-08-30 22:11:43 -0500

    change footer message

--- src/views/landmarks/Footer.tsx
+++ src/views/landmarks/Footer.tsx
@@ -8,7 +8,7 @@ class Footer extends React.PureComponent<IProps, IState> {
     public render(): JSX.Element {
         return (
             <footer className="footer">
-                <p>&copy; {'Company 2017'}</p>
+                <p>{'This footer is a deferred async component. It does not render server-side. It lazy loads on the client-side.'}</p>
             </footer>
         );
     }
