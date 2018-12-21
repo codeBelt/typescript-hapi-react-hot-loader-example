76f4d68787cfed0528312964137b54ca48d6ee67
codeBelt <code@codebelt.com>
2018-12-20 20:36:13 -0600

    remove redux-form

--- src/stores/IStore.ts
+++ src/stores/IStore.ts
@@ -1,13 +1,11 @@
 import {RouterState} from 'connected-react-router';
 import {Store} from 'redux';
-import {FormReducer} from 'redux-form';
 import IMetaReducerState from './meta/IMetaReducerState';
 import IUserReducerState from './user/IUserReducerState';
 import IRenderReducerState from './render/IRenderReducerState';
 import IModalReducerState from './modal/IModalReducerState';
 
 export default interface IStore extends Store<IStore> {
-    readonly form: FormReducer;
     readonly metaReducer: IMetaReducerState;
     readonly modalReducer: IModalReducerState;
     readonly renderReducer: IRenderReducerState;
--- src/stores/rootReducer.ts
+++ src/stores/rootReducer.ts
@@ -2,7 +2,6 @@ import {combineReducers, Reducer, ReducersMapObject} from 'redux';
 import {connectRouter} from 'connected-react-router';
 import UserReducer from './user/UserReducer';
 import MetaReducer from './meta/MetaReducer';
-import {reducer as formReducer} from 'redux-form';
 import RenderReducer from './render/RenderReducer';
 import IStore from './IStore';
 import ModalReducer from './modal/ModalReducer';
@@ -10,7 +9,6 @@ import {History} from 'history';
 
 export default (history: History): Reducer<IStore> => {
     const reducerMap: ReducersMapObject = {
-        form: formReducer,
         metaReducer: MetaReducer.reducer,
         modalReducer: ModalReducer.reducer,
         renderReducer: RenderReducer.reducer,
